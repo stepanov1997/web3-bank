e96a46060227ab98be1930d5e8615aa6c0b7a50f
Kristijan Stepanov <kristijan.stepanov95@gmail.com>
2023-01-31 00:02:35 +0100

    Formated swap contract and deleted lock.

--- hardhat/contracts/Swap.sol
+++ hardhat/contracts/Swap.sol
@@ -1,159 +1,92 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.17;
 pragma abicoder v2;
 
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
-contract SwapExamples {
-    // NOTE: Does not work with SwapRouter02
-    ISwapRouter public constant swapRouter =
-    ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+contract Swap {
+    ISwapRouter public constant swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
 
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
-    /// @notice Swaps a fixed amount of WETH for a maximum possible amount of DAI
-    function swapExactInputSingle(uint amountIn)
-    external
-    returns (uint amountOut)
-    {
-        TransferHelper.safeTransferFrom(
-            WETH9,
-            msg.sender,
-            address(this),
-            amountIn
-        );
+    constructor() {}
+
+    function swapExactInputSingle(uint amountIn) public returns (uint amountOut) {
+        TransferHelper.safeTransferFrom(WETH9, msg.sender, address(this), amountIn);
         TransferHelper.safeApprove(WETH9, address(swapRouter), amountIn);
 
-        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
-        .ExactInputSingleParams({
-        tokenIn: WETH9,
-        tokenOut: DAI,
-        // pool fee 0.3%
-        fee: 3000,
-        recipient: msg.sender,
-        deadline: block.timestamp,
-        amountIn: amountIn,
-        amountOutMinimum: 0,
-        // NOTE: In production, this value can be used to set the limit
-        // for the price the swap will push the pool to,
-        // which can help protect against price impact
-        sqrtPriceLimitX96: 0
-        });
+        ISwapRouter.ExactInputSingleParams memory params;
+        params.tokenIn = WETH9;
+        params.tokenOut = DAI;
+        params.fee = 3000;
+        params.recipient = msg.sender;
+        params.deadline = block.timestamp;
+        params.amountIn = amountIn;
+        params.amountOutMinimum = 0;
+        params.sqrtPriceLimitX96 = 0;
+
         amountOut = swapRouter.exactInputSingle(params);
     }
 
-    /// @notice swaps a minimum possible amount of WETH for a fixed amount of DAI.
-    function swapExactOutputSingle(uint amountOut, uint amountInMaximum)
-    external
-    returns (uint amountIn)
-    {
-        TransferHelper.safeTransferFrom(
-            WETH9,
-            msg.sender,
-            address(this),
-            amountInMaximum
-        );
+    function swapExactOutputSingle(uint amountOut, uint amountInMaximum) public returns (uint amountIn) {
+        TransferHelper.safeTransferFrom(WETH9, msg.sender, address(this), amountInMaximum);
         TransferHelper.safeApprove(WETH9, address(swapRouter), amountInMaximum);
 
-        ISwapRouter.ExactOutputSingleParams memory params = ISwapRouter
-        .ExactOutputSingleParams({
-        tokenIn: WETH9,
-        tokenOut: DAI,
-        fee: 3000,
-        recipient: msg.sender,
-        deadline: block.timestamp,
-        amountOut: amountOut,
-        amountInMaximum: amountInMaximum,
-        sqrtPriceLimitX96: 0
-        });
+        ISwapRouter.ExactOutputSingleParams memory params;
+        params.tokenIn = WETH9;
+        params.tokenOut = DAI;
+        params.fee = 3000;
+        params.recipient = msg.sender;
+        params.deadline = block.timestamp;
+        params.amountOut = amountOut;
+        params.amountInMaximum = amountInMaximum;
+        params.sqrtPriceLimitX96 = 0;
 
         amountIn = swapRouter.exactOutputSingle(params);
 
         if (amountIn < amountInMaximum) {
-            // Reset approval on router
             TransferHelper.safeApprove(WETH9, address(swapRouter), 0);
-            // Refund WETH to user
-            TransferHelper.safeTransfer(
-                WETH9,
-                msg.sender,
-                amountInMaximum - amountIn
-            );
+            TransferHelper.safeTransfer(WETH9, msg.sender, amountInMaximum - amountIn);
         }
     }
 
-    /// @notice swapInputMultiplePools swaps a fixed amount of WETH for a maximum possible amount of DAI
-    /// swap WETH --> USDC --> DAI
-    function swapExactInputMultihop(uint amountIn)
-    external
-    returns (uint amountOut)
-    {
-        TransferHelper.safeTransferFrom(
-            WETH9,
-            msg.sender,
-            address(this),
-            amountIn
-        );
+    function swapExactInputMultihop(uint amountIn) public returns (uint amountOut) {
+        TransferHelper.safeTransferFrom(WETH9, msg.sender, address(this), amountIn);
         TransferHelper.safeApprove(WETH9, address(swapRouter), amountIn);
+        ISwapRouter.ExactInputSingleParams memory params;
+        params.tokenIn = USDC;
+        params.tokenOut = DAI;
+        params.fee = 3000;
+        params.recipient = msg.sender;
+        params.deadline = block.timestamp;
+        params.amountIn = amountIn;
+        params.amountOutMinimum = 0;
+        params.sqrtPriceLimitX96 = 0;
 
-        ISwapRouter.ExactInputParams memory params = ISwapRouter
-        .ExactInputParams({
-        path: abi.encodePacked(
-                WETH9,
-                uint24(3000),
-                USDC,
-                uint24(100),
-                DAI
-            ),
-        recipient: msg.sender,
-        deadline: block.timestamp,
-        amountIn: amountIn,
-        amountOutMinimum: 0
-        });
-        amountOut = swapRouter.exactInput(params);
+        amountOut = swapRouter.exactInputSingle(params);
     }
 
-    /// @notice swapExactOutputMultihop swaps a minimum possible amount of WETH for a fixed amount of USDC
-    /// swap WETH --> USDC --> DAI
-    function swapExactOutputMultihop(uint amountOut, uint amountInMaximum)
-    external
-    returns (uint amountIn)
-    {
-        TransferHelper.safeTransferFrom(
-            WETH9,
-            msg.sender,
-            address(this),
-            amountInMaximum
-        );
-        TransferHelper.safeApprove(WETH9, address(swapRouter), amountInMaximum);
+    function swapExactOutputMultihop(uint amountOut, uint amountInMaximum) public returns (uint amountIn) {
+        TransferHelper.safeTransferFrom(USDC, msg.sender, address(this), amountInMaximum);
+        TransferHelper.safeApprove(USDC, address(swapRouter), amountInMaximum);
 
-        // The parameter path is encoded as (tokenOut, fee, tokenIn/tokenOut, fee, tokenIn)
-        ISwapRouter.ExactOutputParams memory params = ISwapRouter
-        .ExactOutputParams({
-        path: abi.encodePacked(
-                DAI,
-                uint24(100),
-                USDC,
-                uint24(3000),
-                WETH9
-            ),
-        recipient: msg.sender,
-        deadline: block.timestamp,
-        amountOut: amountOut,
-        amountInMaximum: amountInMaximum
-        });
+        ISwapRouter.ExactOutputSingleParams memory params;
+        params.tokenIn = USDC;
+        params.tokenOut = DAI;
+        params.fee = 3000;
+        params.recipient = msg.sender;
+        params.deadline = block.timestamp;
+        params.amountOut = amountOut;
+        params.amountInMaximum = amountInMaximum;
+        params.sqrtPriceLimitX96 = 0;
 
-        amountIn = swapRouter.exactOutput(params);
+        amountIn = swapRouter.exactOutputSingle(params);
+
         if (amountIn < amountInMaximum) {
-            TransferHelper.safeApprove(WETH9, address(swapRouter), 0);
-            TransferHelper.safeTransferFrom(
-                WETH9,
-                address(this),
-                msg.sender,
-                amountInMaximum - amountIn
-            );
+            TransferHelper.safeApprove(USDC, address(swapRouter), 0);
+            TransferHelper.safeTransfer(USDC, msg.sender, amountInMaximum - amountIn);
         }
     }
 }
